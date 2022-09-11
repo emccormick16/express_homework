b2b20a7d35b1df85ccc9cf307b4797246d48e33b
Ed McCormick <edward.mccormick3@gmail.com>
2022-09-11 14:59:19 -0500

    added data

---
+++ dummyData.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
+const data = [];
+
+fs.createReadStream("./dailyActivity_merged.csv")
+  .pipe(
+    parse({
+      delimiter: ",",
+      columns: true,
+      ltrim: true,
+    })
+  )
+  .on("data", function (row) {
+    // 👇 push the object row into the array
+    data.push(row);
+  })
+  .on("error", function (error) {
+    console.log(error.message);
+  })
+  .on("end", function () {
+    // 👇 log the result array
+    console.log("parsed csv data:");
+    console.log(data);
+  });
